7f5b5c34a1aed52a5f618479501ce9fe01e923e6
KingdomThought <79799753+KingdomThought@users.noreply.github.com>
2021-09-05 12:21:36 -0400

    Add files via upload

--- Fibonacci Recursion/src/Fibonacci.java
+++ Fibonacci Recursion/src/Fibonacci.java
@@ -1,6 +1,14 @@
-class Fibonacci
+public class Fibonacci
+
+/**
+ * @author KingdomThought
+ * @param n Number to be Sequenced Recursively by Fibonacci Method
+ * @return Returns the result of number sequenced by Fibonacci Method
+ */
+
 {
     static int fib(int n)
+    
     {
     if (n <= 1)
        return n;
@@ -9,6 +17,8 @@ class Fibonacci
       
     public static void main (String args[])
     {
+    
+    	
     int n = 10;
     System.out.println("The " + n + "th sequence in this fibaacci sequence is " + fib(n));
     }
